fe78eb2f717d96283d4db3ebc41aae8edb499ccd
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-05-07 18:59:45 -0600

    taylor mx-auto

--- src/components/navegacion/NavBar.js
+++ src/components/navegacion/NavBar.js
@@ -6,44 +6,48 @@ import logo from "/./imgs/logo_ts.png";
 
 function NavBar(){
     return(
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link-merch" aria-current="page" href="#">Merch</a>
+    <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link-merch" aria-current="page" href="#">Merch</a>
                             </li>
 
-                            <li class="nav-item-dropdown">
-                                <a class="nav-link-events" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item-dropdown">
+                                <a className="nav-link-events" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Events
                                 </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <li><Link class="dropdown-item" href="#">Action</Link></li>
-                                    <li><Link class="dropdown-item" href="#">Another action</Link></li>
-                                    <li><Link class="dropdown-item" href="#">Something else here</Link></li>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><Link className="dropdown-item" href="#">Action</Link></li>
+                                    <li><Link className="dropdown-item" href="#">Another action</Link></li>
+                                    <li><Link className="dropdown-item" href="#">Something else here</Link></li>
                                 </ul>
-                                <button class="navbar-toggler navbar-toggler-icon" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                                </button>
+                                
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link-newsletter" href="#">Newsletter</a>
+                            <li className="nav-item">
+                                <a className="nav-link-newsletter" href="#">Newsletter</a>
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link-mdn" href="#">TS Midnights</a>
+                            <li className="nav-item">
+                                <a className="nav-link-mdn" href="#">TS Midnights</a>
                             </li>
                             
                             
                         </ul>
-                    </div>
-                    <a class="navbar-brand">
+                        <a className="navbar-brand">
                         <Link to='/'>
-                            <img src={logo} width='200' clas='mx-auto'/>
+                            <img src={logo} width='200' className='mx-auto'/>
                         </Link>
                     </a>
-
+                </div>
+                
         </div>
     </nav>
     );

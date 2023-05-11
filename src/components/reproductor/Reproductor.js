41bd09b3fad9999e59a643b78c62b383b793c71a
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-05-10 20:04:25 -0600

    slide

--- src/components/reproductor/Reproductor.js
+++ src/components/reproductor/Reproductor.js
@@ -11,54 +11,72 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 function Reproductor() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const sliderRef = useRef();
 
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const sliderRef = useRef();
-  
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      initialSlide: currentSlide,
-      beforeChange: (oldIndex, newIndex) => setCurrentSlide(newIndex)
-    };
-  
-    const goToSlide = (index) => {
-      setCurrentSlide(index);
-      sliderRef.current.slickGoTo(index);
-    };
-    
-    return (
-        <div className="Reproductor-container">
-            <Slider ref={sliderRef} {...settings}>
-                <div className="slide">
-                    <div className="video-reproduction">
-                        <   img src={Lavander} alt="Slide 1" />
-                        <div className="text-video">
-                            <h2>LAVANDER HAZE (OFFICIAL MUSIC VIDEO)</h2>
-                        </div>
-                        <div className="play">
-                            <img src={Play} alt="Play button" />
-                        </div>
-                    </div>
-                
-                </div>
-            </Slider>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: currentSlide,
+    beforeChange: (oldIndex, newIndex) => setCurrentSlide(newIndex)
+  };
 
+  const goToSlide = (index) => {
+    setCurrentSlide(index);
+    sliderRef.current.slickGoTo(index);
+  };
 
-            <div className='next-button'>
-                <img
-                    src={next}
-                    alt="Change Slide"
-                    onClick={() => goToSlide((currentSlide + 1) % 3)}
-                    style={{ cursor: 'pointer' }}
-                />
+  return (
+    <div className="Reproductor-container">
+      <Slider ref={sliderRef} {...settings}>
+        <div className="slide">
+          <div className="video-reproduction">
+            <img src={Lavander} alt="Slide 1" />
+            <div className="text-video">
+              <h2>LAVANDER HAZE (OFFICIAL MUSIC VIDEO)</h2>
             </div>
-           
+            <div className="play">
+              <img src={Play} alt="Play button" />
+            </div>
+          </div>
         </div>
-    );
+        <div className="slide">
+          <div className="video-reproduction2">
+            <img src={Anti} alt="Slide 2" />
+            <div className="text-video">
+              <h2>ANTI-HERO (OFFICIAL MUSIC VIDEO)</h2>
+            </div>
+            <div className="play">
+              <img src={Play} alt="Play button" />
+            </div>
+          </div>
+        </div>
+        <div className="slide3">
+          <div className="video-reproduction3">
+            <img src={Beje} alt="Slide 3" />
+            <div className="text-video">
+              <h2>BEJEWELED (OFFICIAL MUSIC VIDEO)</h2>
+            </div>
+            <div className="play">
+              <img src={Play} alt="Play button" />
+            </div>
+          </div>
+        </div>
+      </Slider>
+
+      <div className="next-button">
+        <img
+          src={next}
+          alt="Change Slide"
+          onClick={() => goToSlide((currentSlide + 1) % 3)}
+          style={{ cursor: 'pointer' }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Reproductor;
